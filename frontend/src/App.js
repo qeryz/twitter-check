b05034634cab0784bcfb94cf1fae82aec7507a53
qeryz <33709208+qeryz@users.noreply.github.com>
2022-04-02 23:04:28 -0700

    -update v2
    -fixed formatting
    -added new favicon
    -changed color scheme + fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Typography, TextField, Stack, Button, Paper, Box, Avatar } from '@mui/m
 import { useEffect, useState } from 'react';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
 import TwitterIcon from '@mui/icons-material/Twitter';
 import axios from 'axios';
 
@@ -54,11 +55,14 @@ function App() {
   const [isFollowed, setIsFollowed] = useState(false);
   const [isFollowing, setIsFollowing] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+
   const getInfo = async () => {
+    setLoading(true);
     await getPicture();
 
     try {
-      const response = await axios.get('http://localhost:4000/api/friendships', {
+      const response = await axios.get('https://twitter-check-follow.herokuapp.com/api/friendships', {
         params: {
           user1: user1,
           user2: user2,
@@ -69,23 +73,25 @@ function App() {
       
       // console.log(response.data['relationship'].source);
       setFollowInfo(response.data['relationship'].source);
+      setLoading(false);
 
     } catch (error) {
       console.log("Undefined user");
       setIsFollowed(false);
       setIsFollowing(false);
+      setLoading(false);
     }
 
   };
 
   const getPicture = async () => {
     try {
-      const response1 = await axios.get('http://localhost:4000/api/users', {
+      const response1 = await axios.get('https://twitter-check-follow.herokuapp.com/api/users', {
         params: {
           user: user1,
         }
       })
-      const response2 = await axios.get('http://localhost:4000/api/users', {
+      const response2 = await axios.get('https://twitter-check-follow.herokuapp.com/api/users', {
         params: {
           user: user2,
         }
@@ -107,10 +113,6 @@ function App() {
   };
 
   useEffect(()=>{
-    // if (sourceName === user1 && targetName === user2)
-    //   setIsSubmit(true);
-    // else
-    //   setIsSubmit(false);
 
     const followedBy = followInfo.followed_by;
     const following = followInfo.following;
@@ -127,12 +129,14 @@ function App() {
           <img src={logo} className="logo" alt="Twitter" />
         </a>
         <div>
-          <Typography color='#8cb8d1' variant='h4'>Friendship Check</Typography>
+          <Typography color='#8cb8d1' variant='h5'>TwitFriends</Typography>
         </div>
       </header>
-      <div style={{ display: 'block',}}>
-        <Typography color='#6ea3c1' style={{ marginBottom: '4rem' }} variant='subtitle1'>Check to see if two people follow each other on Twitter.</Typography>
-        <form autoComplete='off' noValidate onSubmit={handleCheck}>
+
+      <div style={{ display: 'block' }}>
+        <Typography color='#6ea3c1' variant='subtitle1'>Check if two people follow each other on Twitter.</Typography>
+        
+        <form style={{ marginTop: '3rem' }} autoComplete='off' noValidate onSubmit={handleCheck}>
         <Stack direction='row' justifyContent="center" alignItems="center" spacing={1}>
           <Typography color='#1DA1F2'>Does</Typography>
           <CssTextField sx={{ input: { color: '#1DA1F2' }, label: { color: '#1DA1F2'} }} id="outlined-basic" 
@@ -143,9 +147,10 @@ function App() {
           <Typography variant='h6' color='#1DA1F2'>?</Typography>
         </Stack>
         <CButton type="submit" disabled={!user1 || !user2 || user1.length < 4 || user2.length < 4} style={{ marginTop: '2rem', marginBottom: '3rem', color: '#1DA1F2' }} 
-        endIcon={<SendIcon />} variant="outlined" onClick={handleCheck}>Check</CButton>
+        endIcon={!loading && <SendIcon />} variant="outlined" onClick={handleCheck}>{!loading ? 'Check' : 'Checking...'}</CButton>
         </form>
-      </div>      
+      </div>    
+        
       {(isSubmit && targetName === user2 && sourceName === user1) && (
       <ThemeProvider theme={darkTheme}>
       <Box
@@ -156,9 +161,10 @@ function App() {
           '& > :not(style)': {
             m: 1,
             padding: {xs: '0.7rem', md: '2rem'},
-            width: {xs: '85vw', md: 'auto'},
+            width: 'auto',
             height: 'auto',
           },
+          marginBottom: '2rem',
         }}
       >
         <Paper style={{ borderRadius: '15px' }} elevation={0}>
@@ -167,11 +173,11 @@ function App() {
             <Typography display = 'inline' variant = 'h5' color = '#66bb6a' >{(isFollowed && isFollowing) && 'friends'}</Typography>
             <Typography display = 'inline' variant = 'h5' color = 'error' >{(!isFollowed || !isFollowing) && 'not friends'}</Typography>
             <Stack justifyContent="center" alignItems="center" display='flex' direction='row' spacing={1}>
-              <Typography variant = 'subtitle1' color = '#1DA1F2' >{isFollowing ? `${sourceName} is following ${targetName}` : `${sourceName} is not following ${targetName}`}</Typography>
+              <Typography variant = 'subtitle2' color = '#1DA1F2' >{isFollowing ? `${sourceName} is following ${targetName}` : `${sourceName} is not following ${targetName}`}</Typography>
               {isFollowing ? <CheckCircleOutlineIcon color='success' /> : <HighlightOffIcon color='error' />}
             </Stack>
             <Stack justifyContent="center" alignItems="center" display='flex' direction='row' spacing={1}>
-              <Typography variant = 'subtitle1' color = '#1DA1F2' >{isFollowed ? `${targetName} is following ${sourceName}` : `${targetName} is not following ${sourceName}`}</Typography>
+              <Typography variant = 'subtitle2' color = '#1DA1F2' >{isFollowed ? `${targetName} is following ${sourceName}` : `${targetName} is not following ${sourceName}`}</Typography>
               {isFollowed ? <CheckCircleOutlineIcon color='success' /> : <HighlightOffIcon color='error' />}
             </Stack>
             <Stack style= {{ marginTop: '1rem'}} direction='column' spacing={0.5}>
@@ -194,6 +200,15 @@ function App() {
         </Paper>
       </Box>
       </ThemeProvider>
+      )}
+      {(isSubmit && targetName === user2 && sourceName === user1) && (
+      <>
+        <Typography style={{ opacity: '50%' }} display = 'inline' color='#6ea3c1' variant='caption'>Created by Marcos Padilla </Typography>
+        <a style={{ opacity: '50%', textDecoration: 'none' }} href='https://twitter.com/czkmaz' target="_blank" rel="noreferrer">
+          <Typography color = '#6ea3c1' variant='caption' display = 'inline' >@czkmaz.</Typography>
+        </a>
+        <Typography style={{ opacity: '50%' }} display = 'block' color='#6ea3c1' variant='caption'>© 2022 Marcos Padilla. Licensed under the Apache License, Version 2.0.</Typography>
+      </>
       )}
     </div>
   );
